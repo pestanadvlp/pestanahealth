9711616b03121223f15b1a1dd4df8f83da6a4663
pestanadvlp <profissional.martin@gmail.com>
2026-02-23 13:51:32 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
+const CACHE_NAME = "treinos-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("treinos-v1").then(cache => {
-      return cache.addAll([
-        [
-      "index.html",
-      "style.css",
-      "script.js",
-      "manifest.json",
-      "icons/icon-192.png",
-      "icons/icon-512.png"
-        ]
-    ]);
-  })
-);
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
